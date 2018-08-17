920c096689a14a9672ede6e322a858c478d933b3
thsong <thsong@localhost>
2018-08-17 12:05:58 +0900

    11장-380p-QnaServiceTest 싱글톤 때문에 나는 에러 수정(정상)

--- src/test/java/next/service/QnaServiceTest.java
+++ src/test/java/next/service/QnaServiceTest.java
@@ -13,32 +13,28 @@ import next.model.Question;
 
 public class QnaServiceTest {
 	private QnaService qnaService;
-	private MockQuestionDao questionDao;
-	private MockAnswerDao answerDao;
 
 	@Before
 	public void setup() {
-		questionDao = new MockQuestionDao();
-		answerDao = new MockAnswerDao();
+		MockQuestionDao questionDao = new MockQuestionDao();
+		Question question = newQuestion(1L, "song");
+		questionDao.insert(question);
+		MockAnswerDao answerDao = new MockAnswerDao();
 		qnaService = QnaService.getInstance(questionDao, answerDao);
 	}
-	
+
 	@Test(expected = CannotDeleteException.class)
 	public void deleteQuestion_없는_질문() throws Exception {
 		qnaService.deleteQuestion(1L, newUser("userId"));
 	}
-	
+
 	@Test(expected = CannotDeleteException.class)
 	public void deleteQuestion_다른_사용자() throws Exception {
-		Question question = newQuestion(1L, "song");
-		questionDao.insert(question);
 		qnaService.deleteQuestion(1L, newUser("userId"));
 	}
-	
+
 	@Test
 	public void deleteQuestion_같은_사용자_답변없음() throws Exception {
-		Question question = newQuestion(1L, "song");
-		questionDao.insert(question);
 		qnaService.deleteQuestion(1L, newUser("song"));
 	}
 
