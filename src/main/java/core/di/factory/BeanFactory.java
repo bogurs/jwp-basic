769cc93ae2b4a644c8a6296b8111227c4a87d43c
thsong <thsong@localhost>
2018-08-17 18:57:59 +0900

    11장-405p-BeanFactory 클래스의 메소드 구현(의존관계에 있는 빈을 생성)

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,11 +1,15 @@
 package core.di.factory;
 
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class BeanFactory {
@@ -25,6 +29,47 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+    	for (Class<?> clazz : preInstanticateBeans) {
+    		if (beans.get(clazz) == null) {
+    			instantiateClass(clazz);
+    		}
+    	}
     }
+
+	private Object instantiateClass(Class<?> clazz) {
+		Object bean = beans.get(clazz);
+		if (bean != null) {
+			return bean;
+		}
+		
+		Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+		if (injectedConstructor == null) {
+			bean = BeanUtils.instantiate(clazz);
+			beans.put(clazz, bean);
+			return bean;
+		}
+		
+		logger.debug("Constructor : {}", injectedConstructor);
+		bean = instantiateConstructor(injectedConstructor);
+		beans.put(clazz, bean);
+		return bean;
+	}
+
+	private Object instantiateConstructor(Constructor<?> constructor) {
+		Class<?>[] pTypes = constructor.getParameterTypes();
+		List<Object> args = Lists.newArrayList();
+		for (Class<?> clazz : pTypes) {
+			Class<?> concreteClazz = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+			if (!preInstanticateBeans.contains(concreteClazz)) {
+				throw new IllegalStateException(clazz + "는 Bean이 아니다.");
+			}
+			
+			Object bean = beans.get(concreteClazz);
+			if (bean == null) {
+				bean = instantiateClass(concreteClazz);
+			}
+			args.add(bean);
+		}
+		return BeanUtils.instantiateClass(constructor, args.toArray());
+	}
 }
