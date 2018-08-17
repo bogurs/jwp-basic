d0ae79af28b43021781c13ab44bbf29e3d5419a9
thsong <thsong@localhost>
2018-08-17 17:41:42 +0900

    11장-386p-Mockito JUnit을 pom.xml에 추가하고 QnaServiceTest에 Mock클래스를 제외하고테스트하기

--- src/test/java/next/service/QnaServiceTest.java
+++ src/test/java/next/service/QnaServiceTest.java
@@ -2,39 +2,47 @@ package next.service;
 
 import static next.model.QuestionTest.newQuestion;
 import static next.model.UserTest.newUser;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.Lists;
 
 import next.CannotDeleteException;
-import next.dao.MockAnswerDao;
-import next.dao.MockQuestionDao;
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
 import next.model.Question;
 
+@RunWith(MockitoJUnitRunner.class)
 public class QnaServiceTest {
+	@Mock
+	private QuestionDao questionDao;
+	@Mock
+	private AnswerDao answerDao;
+	
 	private QnaService qnaService;
 
 	@Before
 	public void setup() {
-		MockQuestionDao questionDao = new MockQuestionDao();
-		Question question = newQuestion(1L, "song");
-		questionDao.insert(question);
-		MockAnswerDao answerDao = new MockAnswerDao();
-		qnaService = QnaService.getInstance(questionDao, answerDao);
+		qnaService = new QnaService(questionDao, answerDao);
 	}
 
 	@Test(expected = CannotDeleteException.class)
 	public void deleteQuestion_없는_질문() throws Exception {
-		qnaService.deleteQuestion(1L, newUser("userId"));
-	}
-
-	@Test(expected = CannotDeleteException.class)
-	public void deleteQuestion_다른_사용자() throws Exception {
+		when(questionDao.findById(1L)).thenReturn(null);
+		
 		qnaService.deleteQuestion(1L, newUser("userId"));
 	}
 
 	@Test
 	public void deleteQuestion_같은_사용자_답변없음() throws Exception {
+		Question question = newQuestion(1L, "song");
+		when(questionDao.findById(1L)).thenReturn(question);
+		when(answerDao.findAllByQuestionId(1L)).thenReturn(Lists.newArrayList());
 		qnaService.deleteQuestion(1L, newUser("song"));
 	}
 
