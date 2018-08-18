c6c66bed9e329b73e8de4f75f4efc456295a4596
thsong <thsong@localhost>
2018-08-18 16:16:36 +0900

    12장-426~432p-BeanDefinition과 BeanFactory 리팩토링하기

--- src/main/java/core/di/factory/BeanDefinition.java
+++ src/main/java/core/di/factory/BeanDefinition.java
@@ -1,15 +1,84 @@
 package core.di.factory;
 
-public class BeanDefinition implements BeanDefinitionRegistry {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Set;
 
+import com.google.common.collect.Sets;
+
+public class BeanDefinition {
+	private Class<?> beanClazz;
+	private Constructor<?> injectConstructor;
+	private Set<Field> injectFields;
+	
 	public BeanDefinition(Class<?> clazz) {
-		// TODO Auto-generated constructor stub
+		this.beanClazz = clazz;
+		injectConstructor = getInjectConstructor(clazz);
+		injectFields = getInjectFields(clazz, injectConstructor);
+	}
+	
+	private Constructor<?> getInjectConstructor(Class<?> clazz) {
+		return BeanFactoryUtils.getInjectedConstructor(clazz);
 	}
 
-	@Override
-	public void registerBeanDefinition(Class<?> clazz, BeanDefinition beanDefinition) {
-		// TODO Auto-generated method stub
+	private Set<Field> getInjectFields(Class<?> clazz, Constructor<?> constructor) {
+		if (constructor != null) {
+			return Sets.newHashSet();
+		}
 		
+		Set<Field> injectFields = Sets.newHashSet();
+		Set<Class<?>> injectProperties = getInjectPropertiesType(clazz);
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			if (injectProperties.contains(field.getType())) {
+				injectFields.add(field);
+			}
+		}
+		return injectFields;
+	}
+
+	private Set<Class<?>> getInjectPropertiesType(Class<?> clazz) {
+		Set<Class<?>> injectProperties = Sets.newHashSet();
+		Set<Method> injectMethods = BeanFactoryUtils.getInjectedMethods(clazz);
+		for (Method method : injectMethods) {
+			Class<?>[] paramTypes = method.getParameterTypes();
+			if (paramTypes.length != 1) {
+				throw new IllegalStateException("DI할 메소드 인자는 하나여야 합니다.");
+			}
+			
+			injectProperties.add(paramTypes[0]);
+		}
+		
+		Set<Field> injectFields = BeanFactoryUtils.getInjectedFields(clazz);
+		for (Field field : injectFields) {
+			injectProperties.add(field.getType());
+		}
+		return injectProperties;
+	}
+
+	public Class<?> getBeanClazz() {
+		return this.beanClazz;
+	}
+
+	public Constructor<?> getInjectConstructor() {
+		return injectConstructor;
+	}
+
+	public Set<Field> getInjectFields() {
+		return this.injectFields;
+	}
+	
+	public InjectType getResolvedInjectMode() {
+		if (injectConstructor != null) {
+			return InjectType.INJECT_CONSTRUCTOR;
+		}
+		
+		if (!injectFields.isEmpty()) {
+			return InjectType.INJECT_FIELD;
+		}
+		
+		return InjectType.INJECT_NO;
 	}
 
 }
--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,65 +1,98 @@
 package core.di.factory;
 
-import java.util.Arrays;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class BeanFactory implements BeanDefinitionRegistry {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
-
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    private List<Injector> injectors;
-    
     private Map<Class<?>, BeanDefinition> beanDefinitions = Maps.newHashMap();
     
-    public BeanFactory() {
-    }
-
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
-
-        injectors = Arrays.asList(new FieldInjector(this), new SetterInjector(this), new ConstructorInjector(this));
-    }
-
-	public Set<Class<?>> getPreInstanticateBeans() {
-        return preInstanticateBeans;
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> T getBean(Class<T> requiredType) {
-        return (T) beans.get(requiredType);
-    }
-
     public void initialize() {
-        for (Class<?> clazz : preInstanticateBeans) {
-            if (beans.get(clazz) == null) {
-                logger.debug("instantiated Class : {}", clazz);
-                inject(clazz);
-            }
-        }
+    	for (Class<?> clazz : getBeanClasses()) {
+			getBean(clazz);
+    	}
     }
+    
+    public Set<Class<?>> getBeanClasses() {
+    	return beanDefinitions.keySet();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public <T> T getBean(Class<T> clazz) {
+    	Object bean = beans.get(clazz);
+    	if (bean != null) {
+    		return (T) bean;
+    	}
+    	
+    	Class<?> concreteClass = findConcreteClass(clazz);
+    	BeanDefinition beanDefinition = beanDefinitions.get(concreteClass);
+    	bean = inject(beanDefinition);
+    	beans.put(concreteClass, bean);
+    	return (T) bean;
+    }
+    
+    private Class<?> findConcreteClass(Class<?> clazz) {
+    	Set<Class<?>> beanClasses = getBeanClasses();
+    	Class<?> concreteClazz = BeanFactoryUtils.findConcreteClass(clazz, beanClasses);
+    	if (!beanClasses.contains(concreteClazz)) {
+    		throw new IllegalStateException(clazz + "는 Bean이 아니다.");
+    	}
+		return concreteClazz;
+	}
 
-    private void inject(Class<?> clazz) {
-        for (Injector injector : injectors) {
-            injector.inject(clazz);
-        }
-    }
+	private Object inject(BeanDefinition beanDefinition) {
+    	if (beanDefinition.getResolvedInjectMode() == InjectType.INJECT_NO) {
+    		return BeanUtils.instantiate(beanDefinition.getBeanClazz());
+    	} else if (beanDefinition.getResolvedInjectMode() == InjectType.INJECT_FIELD) {
+    		return injectFields(beanDefinition);
+    	} else {
+    		return injectConstructor(beanDefinition);
+    	}
+	}
 
-    void registerBean(Class<?> clazz, Object bean) {
-        beans.put(clazz, bean);
-    }
+	private Object injectConstructor(BeanDefinition beanDefinition) {
+		Constructor<?> constructor = beanDefinition.getInjectConstructor();
+		List<Object> args = Lists.newArrayList();
+		for (Class<?> clazz : constructor.getParameterTypes()) {
+			args.add(getBean(clazz));
+		}
+		return BeanUtils.instantiateClass(constructor, args.toArray());
+	}
+	
+	private Object injectFields(BeanDefinition beanDefinition) {
+		Object bean = BeanUtils.instantiate(beanDefinition.getBeanClazz());
+		Set<Field> injectFields = beanDefinition.getInjectFields();
+		for (Field field : injectFields) {
+			injectField(bean, field);
+		}
+		return bean;
+	}
+
+	private void injectField(Object bean, Field field) {
+		logger.debug("Inject Bean : {}, Field : {}", bean, field);
+		try {
+			field.setAccessible(true);
+			field.set(bean, getBean(field.getType()));
+		} catch (IllegalAccessException | IllegalArgumentException e) {
+			logger.error(e.getMessage());
+		}
+	}
 
     void clear() {
-        preInstanticateBeans.clear();
+        beanDefinitions.clear();
         beans.clear();
     }
 
@@ -67,9 +100,5 @@ public class BeanFactory implements BeanDefinitionRegistry {
 	public void registerBeanDefinition(Class<?> clazz, BeanDefinition beanDefinition) {
 		logger.debug("register bean : {}", clazz);
 		beanDefinitions.put(clazz, beanDefinition);
-	}
-
-	public Set<Class<?>> getBeanClasses() {
-		return beanDefinitions.keySet();
 	}
 }
